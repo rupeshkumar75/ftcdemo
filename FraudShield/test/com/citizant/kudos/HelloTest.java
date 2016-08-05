227a9ebb40fe1e90c3e042ed1b668b1df2a58b3a
George Paci <george.paci@santeon.com>
2016-08-05 14:22:37 -0400

    Add JUnit test, test target to ant file, junit & ham crest JARs

---
+++ FraudShield/test/com/citizant/kudos/HelloTest.java
@@ -0,0 +1,13 @@
+package com.citizant.kudos;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class HelloTest {
+
+    @Test
+    public void basicArithmeticShouldWork() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
