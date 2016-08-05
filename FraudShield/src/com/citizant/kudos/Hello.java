c266ba767294a21ba96d8ac560c62f5e8e9caa13
jitendra.kalyandurg <jitendra.kalyandurg@CIT-02562.citizant.corp>
2016-08-05 12:51:58 -0400

    Fixing a compilation error.

--- FraudShield/src/com/citizant/kudos/Hello.java
+++ FraudShield/src/com/citizant/kudos/Hello.java
@@ -3,7 +3,7 @@ package com.citizant.kudos;
 public class Hello {
     
     public static void main(String[] args) {
-        System.out.println("Hello, world!!);
+        System.out.println("Hello, world!!");
     }
 
 }
